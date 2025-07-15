90630596c58d7994ab31acaec84633beeb325f1a
Akuma-RL <akumarocketleague@gmail.com>
2025-07-14 20:41:58 -0700

    added build cleanup

--- modules/system/gc.nix
+++ modules/system/gc.nix
@@ -1,20 +1,25 @@
 { config, pkgs, lib, ... }:
 
 {
- systemd.services.nix-gc-clean = {
-  description = "Nix GC: Keep only the last 5 generations";
-  serviceConfig = {
-   Type = "oneshot";
-   ExecStart = "${pkgs.nix}/bin/nix-env --delete-generations +5 --profile /nix/var/nix/profiles/system";
+  systemd.services.nix-gc-clean = {
+    description = "Nix GC: keep only last 5 generations and clean store";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.writeShellScript "nix-gc-clean" ''
+        ${pkgs.nix}/bin/nix-env --delete-generations +5 --profile /nix/var/nix/profiles/system
+        ${pkgs.nix}/bin/nix-collect-garbage -d
+      ''}";
+    };
   };
- };
 
- systemd.timers.nix-gc-clean = {
-  description = "Run nix GC every day, keeping only 5 generations";
-  wantedBy = [ "timers.target" ];
-  timerConfig = {
-   OnCalendar = "daily";
-   Persistent = true;
+  systemd.timers.nix-gc-clean = {
+    description = "Run nix GC daily";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
   };
- };
+
+  nix.settings.auto-optimise-store = true;
 }
